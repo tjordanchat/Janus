0a16a49bffd12be8d47c870937b102b2c8e3f9c3
Tim Jordan <jamestjordan@gmail.com>
2026-02-15 07:42:04 -0500

    Sun Feb 15 06:30:29 AM EST 2026

--- ergo/pong.ex
+++ ergo/pong.ex
@@ -91,3 +91,34 @@ end
 Hello.world() # :yes
 
 
+######################################
+#
+
+defmodule Accumulator do
+  def loop(anagrams \\ %{}) do
+    receive do
+      {from, {:add, {letters, word}}} ->
+        anagrams = add_word(anagrams, letters, word)
+        send(from, :ok)
+        loop(anagrams) # must put loop/0 inside each match
+
+      {from, :list} ->
+        send(from, {:ok, list_anagrams(anagrams)})
+        loop(anagrams)
+    end
+  end
+
+  defp add_word(anagrams, letters, word) do
+    words = Map.get(anagrams, letters, [])
+
+    anagrams
+    |> Map.put(letters, [word|words])
+  end
+
+  defp list_anagrams(anagrams) do
+    anagrams
+    |> Enum.filter(fn {k, v} -> length(v) >= 3 end)
+  end
+end
+
+
