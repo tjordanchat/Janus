597c492a8a6055151c850b7c12ea164a819a5073
Tim Jordan <jamestjordan@gmail.com>
2026-03-02 13:10:32 -0500

    Mon Mar  2 09:37:03 AM EST 2026

---
+++ ergo/proto.ex
@@ -0,0 +1,133 @@
+# Process A
+
+defmodule MR do
+
+  defp stages = 
+    [
+      { :d_TDD => spawn(d_TDD) },
+      { :d_TPLAN => spawn(d_TPLAN) },
+      { :d_TCASE => spawn(d_TCASE) },
+      { :d_TCOND => spawn(d_TCOND) },
+      { :d_FDD => spawn(d_FDD) },
+      { :d_AFEA => spawn(d_AFEA) }
+    ]
+  end
+  
+end
+
+defp d_BRD do
+    
+
+# Synchronize: Send and wait for reply
+send(pid, {:ping, self()})
+receive do
+  :pong -> IO.puts("Received pong")
+after
+  1000 -> IO.puts("Timed out")
+end
+
+
+defmodule KV do
+  def start_link do
+    Task.start_link(fn -> loop(%{}) end) # Start a new process with an initial state (an empty map)
+  end
+
+  defp loop(map) do
+    receive do
+      {:get, key, caller} ->
+        send(caller, Map.get(map, key)) # Send response back to the caller
+        loop(map) # Recursively call loop with the current state
+
+      {:put, key, value} ->
+        new_map = Map.put(map, key, value)
+        loop(new_map) # Recursively call loop with the updated state
+
+      ->
+        IO.puts("Received unknown message")
+        loop(map)
+    end
+  end
+end
+
+# Example usage in iex:
+# {:ok, pid} = KV.start_link()
+# send(pid, {:put, :key, "value"})
+# send(pid, {:get, :key, self()})
+# flush() # Use flush() in IEx to display messages in the shell's mailbox
+
+#############################################################
+# PRIV/REPO/SEEDS.EXS
+alias MyApp.Districts.District
+alias MyApp.Repo
+
+# 1. Define the path to your JSON file
+json_path = Path.join(:code.priv_dir(:my_app), "static/json/district_list.json")
+
+# 2. Read the file content
+case File.read(json_path) do
+  {:ok, content} ->
+    # 3. Decode the JSON content into an Elixir data structure
+    with {:ok, data} <- Poison.decode(content) do
+      # 4. Loop through the data and insert into the database
+      Enum.each(data, fn item_attrs ->
+        # Create a changeset and insert using Ecto
+        changeset = District.changeset(%District{}, item_attrs)
+
+        case Repo.insert(changeset) do
+          {:ok, district} ->
+            IO.puts("Inserted district: #{district.name}")
+          {:error, reason} ->
+            IO.puts("Failed to insert district: #{item_attrs["name"]} - #{inspect reason}")
+        end
+      end)
+    end
+
+  {:error, reason} ->
+    IO.puts("Error reading file: #{inspect reason}")
+end
+
+
+# Example usage:
+quoted = quote do
+  defmodule Hello do
+    def world, do: :yes
+  end
+end
+
+[{module, binary}] = Code.compile_quoted(quoted)
+# To use it:
+:code.load_binary(module, 'nofile', binary)
+Hello.world() # :yes
+
+
+######################################
+#
+
+defmodule Accumulator do
+  def loop(anagrams \\ %{}) do
+    receive do
+      {from, {:add, {letters, word}}} ->
+        anagrams = add_word(anagrams, letters, word)
+        send(from, :ok)
+        loop(anagrams) # must put loop/0 inside each match
+
+      {from, :list} ->
+        send(from, {:ok, list_anagrams(anagrams)})
+        loop(anagrams)
+    end
+  end
+
+  defp add_word(anagrams, letters, word) do
+    words = Map.get(anagrams, letters, [])
+
+    anagrams
+    |> Map.put(letters, [word|words])
+  end
+
+  defp list_anagrams(anagrams) do
+    anagrams
+    |> Enum.filter(fn {k, v} -> length(v) >= 3 end)
+  end
+end
+
+
